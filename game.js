adf6615ec2fa6b39dbb3f5b7eb6c725f4b4b2369
Seibe TAKAHASHI <t@seibe.jp>
2019-08-23 03:37:48 +0900

    context2d版の仮実装

---
+++ game.js
@@ -0,0 +1,28 @@
+
+import { gc, GameBase } from "./lib/gamecanvas.js";
+
+class Game extends GameBase {
+    /********* 初期化の手順はこちらに *********/
+    initGame() { }
+    enterGame() { }
+
+    /********* フレーム計算処理はこちらに *********/
+    updateGame() { }
+    resizeGame() { }
+
+    /********* フレーム描画処理はこちらに *********/
+    drawGame() {
+        // 画面を白で塗りつぶします
+        gc.clearScreen();
+
+        // ここから、画像を表示する命令を記述していく
+        gc.drawImage("img0.png", 0, 0);
+        gc.setColor(0, 0, 0);
+        gc.drawString("この文字と青空の画像が見えていれば成功です", 60, 220);
+    }
+
+    /********* 終了時の処理はこちらに *********/
+    leaveGame() { }
+    finalGame() { }
+}
+new Game();
